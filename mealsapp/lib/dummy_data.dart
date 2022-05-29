1f3f98a68e6aae1f5c22c728e4581fca272472c2
Akshay Kumar Sah <akki1908@2001gamil.com>
2022-05-29 19:00:22 +0530

    day1

---
+++ mealsapp/lib/dummy_data.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const dummyCategories = [
+  Categroy(id: 'c1', title: 'Italian', color: Colors.purple),
+  Categroy(id: 'c2', title: 'Chinies', color: Colors.green),
+  Categroy(id: 'c3', title: 'Indian', color: Colors.pink),
+  Categroy(id: 'c4', title: 'French', color: Colors.blue),
+];
